a363d75dc151af273f6fa39c677497326f1bce6b
HazukiMiki <xzhou92@gmail.com>
2022-11-22 16:36:42 -0500

    delet more stuff don't need

--- pages/[userId]/status/[statusId].js
+++ pages/[userId]/status/[statusId].js
@@ -1,8 +1,3 @@
-import Head from 'next/head'
-//This page should return twitter.com/profileUsername/status/message id
-import NavBar from '../../../components/NavBar.jsx'
-import SideBar from '../../../components/SideBar.jsx'
-import Tweet from '../../../components/Tweet.jsx'
 
 //This page should return twitter.com/profileUsername/status/message id
 // soo... might have to change stuff
