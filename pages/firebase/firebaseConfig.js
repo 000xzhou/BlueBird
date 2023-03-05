f30e5896167cb758777c06deb1d519f75421b922
HazukiMiki <xzhou92@gmail.com>
2023-03-05 14:13:33 -0500

    .

--- pages/firebase/firebaseConfig.js
+++ pages/firebase/firebaseConfig.js
@@ -7,18 +7,18 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: NEXT_PUBLIC_API_KEY,
-    authDomain: NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: NEXT_PUBLIC_MESSAGE_SENDER_ID,
-    appId: NEXT_PUBLIC_APP_ID,
-    measurementId: NEXT_PUBLIC_MEASUREMENT_ID
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const initFirebase = () => {
     return app
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,97 +1,17 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import { AuthErrorCodes, getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { firebaseApp } from "../services/firebase";
+import { initFirebase } from "../firebase/firebaseConfig";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-function Login() {
-    const [input, setInput] = useState({ email: "", password: "" });
-    const [error, setError] = useState(null);
-
-    // initialised auth instance
-    const auth = getAuth(firebaseApp);
-
-    // handle form submit
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setError("");
-        let email = input.email.toLowerCase().trim();
-        let password = input.password;
-
-        // sign in user
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in
-                console.log(userCredential.user);
-                // ...
-            })
-            .catch((err) => {
-                if (
-                    err.code === AuthErrorCodes.INVALID_PASSWORD ||
-                    err.code === AuthErrorCodes.USER_DELETED
-                ) {
-                    setError("The email address or password is incorrect");
-                } else {
-                    console.log(err.code);
-                    alert(err.code);
-                }
-            });
-    };
-
-    const handleChange = (e) => {
-        setInput((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }));
-    };
-
+const Login = () => {
+    initFirebase()
+    const auth = getAuth()
+    const provider = new GoogleAuthProvider()
+    const signIn = async () => {
+        const result = await signInWithPopup(auth, provider)
+        console.log(result.user)
+    }
     return (
-        <div className="form-body">
-            <form autoComplete="off" className="form" onSubmit={handleSubmit}>
-                <h1>Sign In</h1>
-                <p>Fill the form below to sign in to your account.</p>
-                <div className="email-input">
-                    <input
-                        name="email"
-                        placeholder="Enter email"
-                        type="text"
-                        onChange={handleChange}
-                        value={input.email}
-                        required
-                        autoComplete="true"
-                    />
-                    <label htmlFor="email" className="label-name">
-                        <span className="content-name">Email</span>
-                    </label>
-                </div>
-                <div className="password-input">
-                    <input
-                        name="password"
-                        placeholder="Enter password"
-                        onChange={handleChange}
-                        value={input.password}
-                        type="password"
-                        required
-                        autoComplete="true"
-                    />
-                    <label htmlFor="password" className="label-name">
-                        <span className="content-name">Password</span>
-                    </label>
-                </div>
-                <div className="btn">
-                    {error ? <p className="login-error">{error}</p> : null}
-                    <button title="Login" aria-label="Login" type="submit">
-                        Login
-                    </button>
-                </div>
-            </form>
-            <div className="option">
-                <p>
-                    Don't have an account?
-                    <Link to="/signup">Sign Up</Link>
-                </p>
-            </div>
-        </div>
-    );
+        <button onClick={signIn}>sign in</button>
+    )
 }
 
-export default Login;
+export default Login
